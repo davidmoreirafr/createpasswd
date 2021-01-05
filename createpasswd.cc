2d219560b7ae77f13f2df7555a94c118dbbc8c6a
David Moreira <david@numericore.com>
2021-01-05 13:23:20 +0100

    Generator ipv6 address-ish

--- createpasswd.cc
+++ createpasswd.cc
@@ -9,15 +9,14 @@ extern char *__progname;
 
 void usage(int exit_status) {
   std::cerr << __progname << " [-aehr] [-n number_of_password] [-l min_password_length] " << std::endl
-            << "\t\t[-p distribution_parameter] [-m max_password_length]"
-	    << "\t\t[-s password_length]" << std::endl;
+            << "\t\t[-p distribution_parameter] [-m max_password_length]" << "\t\t[-s password_length]" << std::endl;
   exit(exit_status);
 }
 
-char random_char(const bool number, const bool alpha_num) {
+char random_char(const bool number, const char type_generator) {
   if (number)
     return (char) ('0' + arc4random_uniform(10));
-  if (alpha_num) {
+  if (type_generator == 'a') {
     int rdn = arc4random_uniform(10 + 26 + 26);
     if (rdn < 10)
       return rdn + '0';
@@ -25,49 +24,56 @@ char random_char(const bool number, const bool alpha_num) {
       return rdn - 10 + 'a';
     return rdn - 10 - 26 + 'A';
   }
+  else if (type_generator == 'i') {
+    int rdn = arc4random_uniform(16);
+    if (rdn < 10)
+      return rdn + '0';
+    return rdn - 10 + 'a';
+  }
   return (char) (' ' + arc4random_uniform('~' - ' '));
 }
 
 int main(int argc, char *argv[]) {
   int ch;
-  int min_passwd_len, max_passwd_len;
-  int distribution_param;
+  unsigned min_passwd_len, max_passwd_len;
+  unsigned distribution_param;
   int nb_pass;
 
-  bool display_end, help, random_len, alpha_num_only, number_only;
+  bool display_end, help, random_len, number_only;
+  char type_generator;
 
-  display_end = help = random_len = number_only = alpha_num_only = false;
+  display_end = help = random_len = number_only = false;
   min_passwd_len = max_passwd_len = 12;
   distribution_param = 4;
   nb_pass = 1;
+  type_generator = 0;
 
-  while ((ch = getopt(argc, argv, "adehn:m:l:p:rs:")) != -1)
+  while ((ch = getopt(argc, argv, "adehin:m:l:p:rs:")) != -1)
     switch (ch) {
-      CASE('a', alpha_num_only = true; number_only = false);
-      CASE('d', alpha_num_only = false; number_only = true);
+      CASE('a', type_generator = 'a'; number_only = false);
+      CASE('d', type_generator = 0; number_only = true);
       CASE('e', display_end = true);
       CASE('h', help = true);
+      CASE('i', type_generator = 'i'; number_only = false);
       CASE('n', nb_pass = atoi(optarg));
       CASE('m', max_passwd_len = atoi(optarg));
       CASE('l', min_passwd_len = atoi(optarg));
       CASE('p', distribution_param = atoi(optarg));
       CASE('r', random_len = true);
       CASE('s', max_passwd_len = min_passwd_len = atoi(optarg));
-    default:
-      usage(1);
+    default: usage(1);
     }
 
-  if (help)
-    usage(0);
+  if (help) usage(0);
 
   max_passwd_len = std::max(max_passwd_len, min_passwd_len);
 
   for (int j = 0; j < nb_pass; ++j) {
     for (int i = min_passwd_len + arc4random_uniform(max_passwd_len - min_passwd_len); i != 1; --i)
-      std::cout << random_char(number_only, alpha_num_only);
+      std::cout << random_char(number_only, type_generator);
     if (random_len)
       while (arc4random_uniform(distribution_param))
-	std::cout << random_char(number_only, alpha_num_only);
+	std::cout << random_char(number_only, type_generator);
 
     std::cout << (display_end ? "$" : "") << std::endl;
   }
