54dbdec07d53bbe0229a0589f15a5e74500747df
David Moreira <dmoreira@gopro.com>
2021-05-11 13:43:11 +0000

    build1&2

--- default.nix
+++ default.nix
@@ -1,7 +1,25 @@
 let
   pkgs = import <nixpkgs> {};
   jobs = {
-    build = let
+    build1 = let
+      pkgs = import <nixpkgs> {};
+    in
+      pkgs.releaseTools.nixBuild {
+        name = "createpasswd";
+        src = ./.;
+        buildInputs = (with pkgs; [
+          ninja
+          gcc
+        ]); # comment
+
+        configurePhase = ''
+          ninja -vt clean
+        '';
+        buildPhase = ''
+          ninja -j1
+        '';
+      };
+    build2 = let
       pkgs = import <nixpkgs> {};
     in
       pkgs.releaseTools.nixBuild {
