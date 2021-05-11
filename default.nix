4ef4d296ec088f43dbf1db33481ee476399debd5
David Moreira <dmoreira@gopro.com>
2021-05-11 13:46:22 +0000

    Add a function.

--- default.nix
+++ default.nix
@@ -1,42 +1,28 @@
 let
   pkgs = import <nixpkgs> {};
+  build_function = platform:
+    let
+      pkgs = import <nixpkgs> {};
+    in
+      pkgs.releaseTools.nixBuild {
+        name = "createpasswd";
+        src = ./.;
+        buildInputs = (with pkgs; [
+          ninja
+          gcc
+        ]); # comment
+
+        configurePhase = ''
+          ninja -vt clean
+        '';
+        buildPhase = ''
+          ninja -j1
+        '';
+      };
+
   jobs = {
-    build1 = let
-      pkgs = import <nixpkgs> {};
-    in
-      pkgs.releaseTools.nixBuild {
-        name = "createpasswd";
-        src = ./.;
-        buildInputs = (with pkgs; [
-          ninja
-          gcc
-        ]); # comment
-
-        configurePhase = ''
-          ninja -vt clean
-        '';
-        buildPhase = ''
-          ninja -j1
-        '';
-      };
-    build2 = let
-      pkgs = import <nixpkgs> {};
-    in
-      pkgs.releaseTools.nixBuild {
-        name = "createpasswd";
-        src = ./.;
-        buildInputs = (with pkgs; [
-          ninja
-          gcc
-        ]); # comment
-
-        configurePhase = ''
-          ninja -vt clean
-        '';
-        buildPhase = ''
-          ninja -j1
-        '';
-      };
+    build1 = build_function "toto1";
+    build2 = build_function "toto2";
   };
 in
   jobs
