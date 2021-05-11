61b8e984e49e45c9a062cb0219865b71b9e9db2c
David Moreira <dmoreira@gopro.com>
2021-05-11 16:22:45 +0000

    toto

--- default.nix
+++ default.nix
@@ -11,40 +11,34 @@ let
       pkgs.gcc10
     else
       pkgs.gcc9;
-  build_function = target: (comp:
+  build_function = target: comp:
     let
       pkgs = import <nixpkgs> {
-        system = "x86_64-linux";
+        system = target;
       };
     in
       pkgs.releaseTools.nixBuild {
         name = "createpasswd";
         src = ./.;
-        buildInputs = [pkgs.ninja comp];
-#        buildInputs = (with pkgs; [
-#          "ninja"
-#          gcc9
-#        ]);
-#
+        buildInputs = [
+          pkgs.ninja
+          comp
+        ];
         configurePhase = ''
           ninja -vt clean
         '';
         buildPhase = ''
           ninja -j1
         '';
-      }
-  );
+      };
 in {
-  build = pkgs.lib.genAttrs ["gcc10" "gcc9"]
-    (comp: (
-      pkgs.lib.genAttrs supportedSystems
-        (target: build_function (compiler_conversion comp))
+  build = pkgs.lib.genAttrs [ "foo" ]
+    (target: 
+      pkgs.lib.genAttrs [ "gcc10" "gcc9" ]
+        (comp:
+          build_function "foo" pkgs.gcc10
+                #(compiler_conversion comp)))
     )
-#    (
-#      pkgs.lib.genAttrs supportedSystems
-#        comp: (
-#          target: build_function target (compiler_conversion)
-#        )
-#          #build_function (compiler_conversion comp)
+
     );
 }
