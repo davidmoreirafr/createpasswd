cfdf004dcc9478d2e920d4e60807ff16dbb3478e
David Moreira <dmoreira@gopro.com>
2021-05-11 16:11:14 +0000

    toto

--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
 {
-  supportedSystems ? ["x86_64-linux"]
-  , supportedCompilers ? [ "gcc10" "gcc9" ]
+  supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
+, supportedCompilers ? [ "gcc10" "gcc9" ]
 }:
 with  (import <nixpkgs/pkgs/top-level/release-lib.nix> {
   inherit supportedSystems;
 });
 let
   compiler_conversion = comp:
-    (if comp == "gcc10"
-     then pkgs.gcc10
-     else pkgs.gcc9
-    );
-  build_function = my_compiler:
+    if comp == "gcc10" then
+      pkgs.gcc10
+    else
+      pkgs.gcc9;
+  build_function = target: (comp:
     let
       pkgs = import <nixpkgs> {
         system = "x86_64-linux";
@@ -20,7 +20,7 @@ let
       pkgs.releaseTools.nixBuild {
         name = "createpasswd";
         src = ./.;
-        buildInputs = [pkgs.ninja my_compiler];
+        buildInputs = [pkgs.ninja comp];
 #        buildInputs = (with pkgs; [
 #          "ninja"
 #          gcc9
@@ -32,9 +32,19 @@ let
         buildPhase = ''
           ninja -j1
         '';
-      };
+      }
+  );
 in {
   build = pkgs.lib.genAttrs ["gcc10" "gcc9"]
-    (comp: build_function (compiler_conversion comp)
+    (comp: (
+      pkgs.lib.genAttrs supportedSystems
+        (target: build_function (compiler_conversion comp))
+    )
+#    (
+#      pkgs.lib.genAttrs supportedSystems
+#        comp: (
+#          target: build_function target (compiler_conversion)
+#        )
+#          #build_function (compiler_conversion comp)
     );
 }
