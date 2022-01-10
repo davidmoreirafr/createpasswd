b6a6378bd6de7ab6c0f58177b87394c572578310
David Moreira <dmoreira@gopro.com>
2022-01-10 11:36:17 +0100

    Clean and add gcc9

--- release.nix
+++ release.nix
@@ -1,11 +1,5 @@
-{ supportedSystems ? [
-  "x86_64-linux"
-]}:
-with import <nixpkgs/pkgs/top-level/release-lib.nix>(
-  {
-    inherit supportedSystems;
-  }
-);
+{ supportedSystems ? [ "x86_64-linux" ] }:
+with import <nixpkgs/pkgs/top-level/release-lib.nix>( { inherit supportedSystems; } );
 
 let
   deriv = target: env:
@@ -20,16 +14,13 @@ let
         buildInputs = [
           pkgs.ninja
           pkgs.gcc
-          pkgs.file
         ];
 
         configurationPhase = ''
           ninja -t clean
         '';
         buildPhase = ''
-          echo == Build phase ==
           ninja -v
-          echo == End of Build phase ==
         '';
         installPhase = ''
           mkdir -p $out/bin
@@ -37,32 +28,7 @@ let
         '';
       };
 in {
+  gcc9  = deriv "x86_64-linux" pkgs.gcc9Stdenv;
   gcc10 = deriv "x86_64-linux" pkgs.gcc10Stdenv;
   gcc11 = deriv "x86_64-linux" pkgs.gcc11Stdenv;
 }
-
-#let
-#  pkgs = import <nixpkgs> {};
-#
-#  jobs = rec {
-#
-#    tarball =
-#      pkgs.releaseTools.sourceTarball {
-#        name = "hello-tarball";
-#        src = <hello>;
-#        buildInputs = (with pkgs; [ gettext texLive texinfo ]);
-#      };
-#
-#    build =
-#      { system ? builtins.currentSystem }:
-#
-#      let pkgs = import <nixpkgs> { inherit system; }; in
-#      pkgs.releaseTools.nixBuild {
-#        name = "hello";
-#        src = jobs.tarball;
-#        configureFlags = [ "--disable-silent-rules" ];
-#      };
-#  };
-#in
-#  jobs
-#
